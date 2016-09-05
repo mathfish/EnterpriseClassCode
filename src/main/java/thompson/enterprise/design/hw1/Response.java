6f37cbf4d097521b230f4552a15e889c31c91b9f
Jonathan Thompson <jonathanandmollyt@gmail.com>
2016-09-05 10:55:47 -0400

    Implemented method respondHowIsYourSpouse, along with helper methods. All tests pass for class.

--- src/main/java/thompson/enterprise/design/hw1/Response.java
+++ src/main/java/thompson/enterprise/design/hw1/Response.java
@@ -1,5 +1,7 @@
 package thompson.enterprise.design.hw1;
 
+import java.time.LocalDate;
+
 public class Response {
 
     public String respondToGreeting(Greeting greeting){
@@ -34,6 +36,49 @@ public class Response {
     }
 
     public String respondHowIsYourSpouse(Person person){
-        return "";
+        String response="";
+        int dayOfYear = LocalDate.now().getDayOfYear();
+        int birthDayOfYear = person.getBirthdate().getDayOfYear();
+        int anninversaryDayOfYear = person.getAnniversary().getDayOfYear();
+        boolean bDayComing = birthDayOfYear - dayOfYear >=0 && birthDayOfYear - dayOfYear <= 14;
+        boolean bDayPast = dayOfYear - birthDayOfYear > 0 && dayOfYear -birthDayOfYear <= 14;
+        boolean annDayComing = anninversaryDayOfYear - dayOfYear >=0 && anninversaryDayOfYear - dayOfYear <= 14;
+        boolean annDayPast = dayOfYear - anninversaryDayOfYear > 0 && dayOfYear - anninversaryDayOfYear <= 14;
+
+        if(bDayComing){
+            response = "Isn't " + person.getFirstName() + "'s birthday coming up? Please wish " + himOrHer(person) +
+                    " a happy birthday for me." ;
+        } else if(bDayPast){
+            response = "Didn't " + person.getFirstName() + " just have a birthday? Please wish " + himOrHer(person) +
+                    " a happy birthday for me.";
+        }
+
+        if(annDayComing){
+            if(bDayComing || bDayPast){
+                response = response + " And isn't your anniversary coming up? Happy anniversary!";
+            } else{
+                response = "Isn't your anniversary coming up? Happy anniversary!";
+            }
+        } else if(annDayPast){
+            if(bDayComing || bDayPast){
+                response = response + " And didn't " + person.getFirstName() +
+                        " just have an anniversary? Happy anniversary to you both!";
+            } else{
+                response = "Didn't " + person.getFirstName() +
+                        " just have an anniversary? Happy anniversary to you both!";
+            }
+        } else if(response.equals("")){
+            response = heOrShe(person) + " is doing well, thank you.";
+        }
+
+        return response;
+    }
+
+    private String heOrShe(Person person){
+        return person.getSex().equals(Sex.M) ? "He" : "She";
+    }
+
+    private String himOrHer(Person person){
+        return person.getSex().equals(Sex.M) ? "him" : "her";
     }
 }
