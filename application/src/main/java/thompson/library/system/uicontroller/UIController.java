04ef4963f9538050a52a8238447359efe1ba9c09
Jonathan Thompson <jonathanandmollyt@gmail.com>
2016-12-04 18:53:00 -0500

    Initial implementation of MVC for assignment 8. Unsure if it is working

---
+++ application/src/main/java/thompson/library/system/uicontroller/UIController.java
@@ -0,0 +1,32 @@
+package thompson.library.system.uicontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+
+@Controller
+public class UIController {
+
+    @RequestMapping("/CreatePatron")
+    public String createPatronRequest(@RequestParam(value="firstname", defaultValue = "testfirst") String firstname,
+                                      @RequestParam(value = "lastname", defaultValue = "testlast") String lastname,
+                                      @RequestParam(value = "city", defaultValue = "testcity") String city,
+                                      @RequestParam(value = "state", defaultValue = "AA") String state,
+                                      @RequestParam(value = "zip", defaultValue = "11111") String zip,
+                                      @RequestParam(value = "address", defaultValue = "testaddress") String address,
+                                      @RequestParam(value="email", defaultValue="testemail@rest") String email,
+                                      @RequestParam(value = "phone", defaultValue = "1111111111") String phone,
+                                      @RequestParam(value = "password", defaultValue = "pword") String password,
+                                      Model model){
+        RestTemplate restTemplate = new RestTemplate();
+        String args = "firstname="+firstname+"&lastname="+lastname+"&city="+city+"&state="+state+"&zip="+zip+
+                "&address="+address+"&email="+email+"?phone="+phone+"&password="+password;
+        Response response = restTemplate.getForObject("http://localhost:8080/application/patron?"+args, Response.class);
+        String message = "Response Code["+response.getId()+"]: "+response.getContent();
+        model.addAttribute("message", message);
+        return "greeting";
+    }
+}
