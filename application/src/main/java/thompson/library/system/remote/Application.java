802a06079e6b913ad2e2059b4ef6d6fe3ce36fb6
Jonathan Thompson <jonathanandmollyt@gmail.com>
2016-12-04 22:01:14 -0500

    Cleaned up build file and imports

--- application/src/main/java/thompson/library/system/remote/Application.java
+++ application/src/main/java/thompson/library/system/remote/Application.java
@@ -1,6 +1,5 @@
 package thompson.library.system.remote;
 
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,22 +7,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.remoting.caucho.HessianServiceExporter;
-import org.springframework.transaction.PlatformTransactionManager;
 import thompson.library.system.daos.BranchItemCheckoutDao;
 import thompson.library.system.dtos.PatronDto;
 import thompson.library.system.remote.hessian.PatronService;
 import thompson.library.system.services.BranchServices;
 import thompson.library.system.uicontroller.UIController;
 import thompson.library.system.utilities.LoggingInterceptor;
-
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Properties;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses = {BranchItemCheckoutDao.class, BranchServices.class, LoggingInterceptor.class,
